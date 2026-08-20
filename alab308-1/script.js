af8af4fd150e974de0ed195cd94c84568e231e8c
angelalita77 <anjeanette@gmail.com>
2026-08-20 15:26:22 -0400

    Completed Part 2. Finishing the newIsOver25 boolean variable

--- alab308-1/script.js
+++ alab308-1/script.js
@@ -26,6 +26,7 @@ const isOver25 = !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25);
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
+// ----------- Start of Part 1 Assignmnet ---------------------
 // Check five: All numbers are divisible by 5
 // This is a simple check using modulus and the AND operator.
 const isDivisibleBy5 = n1%5 == 0 && n2%5 == 0 && n3%5 == 0 && n4%5 == 0;
@@ -43,6 +44,7 @@ let answer = [ (n2 - n1) * n3] / n4;
 
 // New isOver25
 // Improve isOver without using the NOT (!) logical function
+const newIsOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
 
 
 // Here, we put the results into a single variable 
@@ -51,9 +53,13 @@ let answer = [ (n2 - n1) * n3] / n4;
 // "isUnder25" as an alternative.
 const isValid = isSum50 && isTwoOdd && isOver25 && isUnique && isDivisibleBy5;
 
+
 // Finally, log the results.
-console.log (isFirstLargerThanLast)
-console.log (isDivisibleBy5);
+let allNums = `${n1}, ${n2}, ${n3}, and ${n4}`;
+
+console.log (`Are numbers ${allNums} divisible by 5? ${String(isDivisibleBy5).toUpperCase()}`)
+console.log (`Is the first number in ${allNums} larger than the last? ${String(isFirstLargerThanLast).toUpperCase()}`);
+console.log (`The following calculation is [ (${n2} - ${n1}) * ${n3} ] / ${n4} = ${answer}`)
 console.log(isValid);
 console.log (answer);
 
@@ -62,20 +68,44 @@ console.log (answer);
 const totalDistance = 1500;
 let fuelBudget = 175
 let averageFuelCost = 3;
-let mph55Speed = 30;
-let mph60Speed = 28;
-let mph75Speed = 23;
+let mpgFrom55MPH = 30;
+let mpgFrom60MPH = 28;
+let mpgFrom75MPH = 23;
 
 // How many gallons of fuel will you need for the entire trip?
-let gallonsAt55mph = totalDistance / mph55Speed;
-let gallonsAt60mph = totalDistance / mph60Speed;
-let gallonsAt75mph = totalDistance / mph75Speed;
+let gallonsAt55mph = totalDistance / mpgFrom55MPH;
+let gallonsAt60mph = totalDistance / mpgFrom60MPH;
+let gallonsAt75mph = totalDistance / mpgFrom75MPH;
 
-console.log(`The number of gallons of gas used on trip when traffling 55MPH is ${gallonsAt55mph}`);
-console.log("The number of gallons of gas used on trip when traffling 60MPH is " + totalDistance / mph60Speed);
 
-let something = totalDistance + fuelBudget;
+console.log(`The number of gallons of gas used on trip when traveling 55MPH is ${gallonsAt55mph} gallons.`);
+console.log(`The number of gallons of gas used on trip when traveling 60MPH is ${gallonsAt60mph} gallons.`);
+console.log(`The number of gallons of gas used on trip when traveling 75MPH is ${gallonsAt75mph} gallons.`);
 
+
+
+//Will your budget be enough to cover the fuel expense?
+let fuelCostA = averageFuelCost * gallonsAt55mph;
+let fuelCostB = averageFuelCost * gallonsAt60mph;
+let fuelCostC = averageFuelCost * gallonsAt75mph;
+
+console.log(`The only travel that my budget will not cover is the fuel expense \
+of ${fuelCostC.toFixed(2)} if traveling at 75PMH`);
+
+fuelCostB;
+fuelCostC;
+
+// How long will the trip take, in hours?
+let tripATime = totalDistance/55;
+let tripBTime = totalDistance/60;
+let tripCTime = totalDistance/75;
+
+tripATime;
+tripBTime;
+tripCTime;
+
+// Compare the results when traveling at an average 
+// of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
 
 
 // Here's another example of how this COULD be done,
